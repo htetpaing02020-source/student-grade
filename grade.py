d83daeac0c53f4cb1128ace22acd47f4e15d0488
htetpaing02020-source <htetpaing02020@gmail.com>
2026-09-14 13:27:09 +0630

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,6 +1,14 @@
 name = input("Enter student name: ")
 mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
-average = ( mark1 + mark) /2
-print("Stdent Name: " ,name)
-print("Average Mark:" ,average)
+
+average = (mark1 + mark2) / 2
+
+print("Student Name:", name)
+print("Average Mark:", average)
+
+if average >= 50:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
+
