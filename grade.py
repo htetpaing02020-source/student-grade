a53f8bace92208aadea27661fead927d8f216572
Htet Paing <htetpaing02020@gmail.com>
2026-09-14 13:19:47 +0630

    Initial student grade program

---
+++ grade.py
@@ -0,0 +1,6 @@
+name = input("Enter student name: ")
+mark1 = float(input("Enter first subject mark: "))
+mark2 = float(input("Enter second subject mark: "))
+average = ( mark1 + mark) /2
+print("Stdent Name: " ,name)
+print("Average Mark:" ,average)
